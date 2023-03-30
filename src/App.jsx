7b01afab3063f1cd7b103560b85a3e3eec4059f9
Aaron MV <epsilon11101@gmail.com>
2023-03-30 13:33:36 -0600

    feat: added transactionhistory components and json data

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
 import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import user from "./json/user.json";
 import data from "./json/data.json";
 import friends from "./json/friends.json";
+import transactions from "./json/transactions.json";
 
 import "./App.css";
 
@@ -23,6 +25,7 @@ function App() {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </section>
   );
 }
